166b8a624f64d6aadc3cb274e398be5469ad3e70
Mohammad Ali <m.aliadnanrauf@gmail.com>
2025-12-31 21:43:59 +0500

    switch to integer type for functions other than division in math.c

--- src/math.c
+++ src/math.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int addFunc(int first_number, int second_number) {
-    return (float)first_number + second_number;
+    return first_number + second_number;
 }
 
 int subFunc(int first_number, int second_number) {
-    return (float)first_number - second_number;
+    return first_number - second_number;
 }
 
 int multiplyFunc(int first_number, int second_number) {
-    return (float)first_number * second_number;
+    return first_number * second_number;
 }
 
 float divideFunc(int first_number, int second_number) {
@@ -20,7 +20,7 @@ int main(void) {
     int sum = addFunc(20, 110);
     int sub = subFunc(20, 110);
     int multi = multiplyFunc(20, 110);
-    float div = divideFunc(20, 110);
+    float div = divideFunc(30.3, 110.25);
     printf("Addition: %d\n", sum);
     printf("Subtraction: %d\n", sub);
     printf("Multiplication: %d\n", multi);
